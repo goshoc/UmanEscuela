fbf2a64a1e034328b5cccec58d447ad47e22c7d8
goshoc <shocron.diego@gmail.com>
2017-06-06 11:49:38 -0300

    Added html

--- est/enviar_datos.php
+++ est/enviar_datos.php
@@ -1,72 +1,93 @@
-<?php
-session_start();
-include_once "../conexion.php";
-if($_SESSION['id_tipo_usuario']==2)
-{
-	$numero = count($_POST);
-	$tags = array_keys($_POST);// obtiene los nombres de las varibles
-	$valores = array_values($_POST);// obtiene los valores de las varibles
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+		<meta name="description" content="">
+		<meta name="author" content="">
+	 <!--
+		Cuando este lista, cambiar favicon y descomentarlo
+	 <link rel="icon" href="../../favicon.ico"> -->
+		<title>Uman</title>
+		<!-- Bootstrap core CSS -->
+		<link href="../css/bootstrap.min.css" rel="stylesheet">
+		<!-- Custom styles for this template -->
+		<link href="../css/signin.css" rel="stylesheet">
+		<link href="../css/indexEst.css" rel="stylesheet">
+		<!-- Custom style for navbar -->
+		<link href="../css/navbar-fixed-top.css" rel="stylesheet">
+		<!-- FONTS-->
+	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
 
-for($j=0;$j<$_POST["cantTablas"];$j++)
-{
-	$fechaTabla[$j] = $_POST["fechaTabla".$j];
-}
-for($l=0;$l<count($fechaTabla);$l++)
-{
-	for($k=0;$k<count($tags);$k++)
-	{
-		if($tags[$k]==$fechaTabla[$l])
+	</head>
+	<body>
+		<!-- Fixed navbar -->
+	    <nav class="navbar navbar-default navbar-fixed-top">
+	      <div class="container">
+	        <div class="navbar-header">
+	          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	          </button>
+	          <a class="navbar-brand" href="index.php">Uman Menú</a>
+	        </div>
+	        <div id="navbar" class="navbar-collapse collapse">
+	          <ul class="nav navbar-nav">
+	            <li class="active"><a href="index.php">Home</a></li>
+	          </ul>
+	          <ul class="nav navbar-nav navbar-right">
+	          <li><a href="LogOut.php">Log out</a></li>
+	          </ul>
+	        </div><!--/.nav-collapse -->
+	      </div>
+	    </nav>
+			<div class='container'>
+		<?php
+		session_start();
+		include_once "../conexion.php";
+		if($_SESSION['id_tipo_usuario']==2)
 		{
-			//echo"(\"INSERT INTO `pedido`	(`fecha`, `id_usuario`, `id_menu`, `comentario`) VALUES (\"".$fechaTabla[$l]."\",\"".$_SESSION['id']."\",".$valores[$k].",\"".$valores[$k+1]."\"); \");";
-			$sql= ("INSERT INTO `pedido`	(`fecha`, `id_usuario`, `id_menu`, `comentario`) VALUES (\"".$fechaTabla[$l]."\",\"".$_SESSION['id']."\",".$valores[$k].",\"".$valores[$k+1]."\"); ");
+			$numero = count($_POST);
+			$tags = array_keys($_POST);// obtiene los nombres de las varibles
+			$valores = array_values($_POST);// obtiene los valores de las varibles
 
-			$result = $con->query($sql);
-			if ( false===$result )
+			for($j=0;$j<$_POST["cantTablas"];$j++)
 			{
-					echo("</br></br>");
-					printf("error: %s\n", mysqli_error($con));
-					echo("</br></br>");
+				$fechaTabla[$j] = $_POST["fechaTabla".$j];
 			}
-			else
+			for($l=0;$l<count($fechaTabla);$l++)
 			{
-					echo ("Registrado el menú del día: ".$fechaTabla[$l].".</br>" );
-				}
-		}
-		}
-	}
-}
-
-/*
-
-	// crea las variables y les asigna el valor
-	for($i=0;$i<$numero;$i++)
-	{
-	//$$tags[$i]=$valores[$i];
-	}
-
-
-
-			for($i=0;$i<$numero;$i=$i+2)
-			{
-				$sql= ("INSERT INTO `pedido`	(`fecha`, `id_usuario`, `id_menu`, `comentario`) VALUES (\"".$tags[$i]."\",\"".$_SESSION['id']."\",".$valores[$i].",\"".$valores[$i+1]."\"); ");
-
-				$result = $con->query($sql);
-				if ( false===$result )
+				for($k=0;$k<count($tags);$k++)
 				{
-					 	echo("</br></br>");
- 	 					printf("error: %s\n", mysqli_error($con));
- 	 					echo("</br></br>");
+					if($tags[$k]==$fechaTabla[$l])
+					{
+						//echo"(\"INSERT INTO `pedido`	(`fecha`, `id_usuario`, `id_menu`, `comentario`) VALUES (\"".$fechaTabla[$l]."\",\"".$_SESSION['id']."\",".$valores[$k].",\"".$valores[$k+1]."\"); \");";
+						$sql= ("INSERT INTO `pedido`	(`fecha`, `id_usuario`, `id_menu`, `comentario`) VALUES (\"".$fechaTabla[$l]."\",\"".$_SESSION['id']."\",".$valores[$k].",\"".$valores[$k+1]."\"); ");
+
+						$result = $con->query($sql);
+						if ( false===$result )
+						{
+								echo("</br></br>");
+								printf("error: %s\n", mysqli_error($con));
+								echo("</br></br>");
+						}
+						else
+						{
+								echo ("Registrado el menú del día: ".$fechaTabla[$l].".</br>" );
+							}
+					}
 				}
-				else
-				{
-  					echo ("Registrado el menú del día: ".$tags[$i].".</br>" );
-  				}
 			}
-			*/
+		}
+		else
+		{
+			echo"Usted no tiene permiso para entrar a esta área";
 
-else
-{
-	echo"Usted no tiene permiso para entrar a esta área";
-
-}
-?>
+		}
+		?>
+		</div>
+	</body>
+</html>
